8aacbefb31385f844fdfdc86728db0440142a646
lucasaec <lucaseac@hawaii.edu>
2016-02-22 02:20:24 -1000

    I added a few things, I made a few assumptions, I assumed that Honolulu will always be selected for a time zone

--- main.java
+++ main.java
@@ -21,6 +21,8 @@ public class main {
 	   */
 	   boolean exit = true;
 	   Scanner scan = new Scanner(System.in);
+	   boolean fileMade = false;
+	   
 	   PrintWriter writer;
 	   BufferedWriter buffWriter;
 	   /* Begin running the interface*/
@@ -34,15 +36,69 @@ public class main {
 		   option = scan.nextInt();
 		   if(option == 1) { // Add an event
 			   try {
-				   scan.nextLine();
-				   System.out.print("Please enter your email: ");
-				   response = scan.nextLine();
-				   File icsFile = new File(response + ".ics");
-			       writer = new PrintWriter(icsFile);
-			       buffWriter = new BufferedWriter(writer);			     
-			       buffWriter.write("BEGIN:VCALENDAR");
-			       System.out.println("Please enter the date");
-			       buffWriter.close();
+				 
+				   
+				   if(!fileMade) {
+					   scan.nextLine();
+					   System.out.print("Please enter your email: ");					   
+					   response = scan.nextLine();
+					   String icsFile = response + ".ics";
+					   writer = new PrintWriter(icsFile);
+			       	   buffWriter = new BufferedWriter(writer);		
+			       	   fileMade = true;
+			           System.out.print("Please enter the name of your calender: ");
+			           response = scan.nextLine();
+			           buffWriter.write("BEGIN:VCALENDAR\n" +
+						       "PRODID:-//Google Inc//Google Calendar 70.9054//EN\n" +
+							   "VERSION:2.0\n" +
+							   "CALSCALE:GREGORIAN\n" +
+							   "METHOD:PUBLISH\n" +
+							   "X-WR-CALNAME:" + response +"\n" +
+							   "X-WR-TIMEZONE:Pacific/Honolulu\n" +
+							   "BEGIN:VTIMEZONE\n" +
+							   "TZID:Pacific/Honolulu\n" +
+							   "X-LIC-LOCATION:Pacific/Honolulu\n" +
+							   "BEGIN:STANDARD\n" +
+							   "TZOFFSETFROM:-1000\n" +
+							   "TZOFFSETTO:-1000\n" +
+							   "TZNAME:HST\n" +
+							   "DTSTART:19700101T000000\n" +
+							   "END:STANDARD\n" +
+							   "END:VTIMEZONE\n" +
+							   "BEGIN:VEVENT\n"); 
+			           /*date start
+			            * might need to add variables for month and year I don't think 
+			            * events last longer than a month*/
+						System.out.println("Please enter the year this starts in 4 integers");
+						response = scan.nextLine();
+						buffWriter.write("DTSTART;TZID=Pacific/Honolulu:" + response);
+						System.out.println("Please enter the month this starts in 2 integers followed by the day,"
+								+ "place 0's when necessary, for example July 4 would be 0704");
+						response = scan.nextLine();
+						buffWriter.write(response + "T");
+						/** I need to double check this one*/
+						System.out.println("Enter the start time in 4 digits, after 12:00pm is 13:00, " + 
+								"add zeros where necessary");
+						response = scan.nextLine();
+						buffWriter.write(response + "00\n");
+						
+						/*date end*/ 
+						/*System.out.println("Please enter the year this ends in 4 integers");
+						response = scan.nextLine();
+						buffWriter.write("DTEND;TZID=Pacific/Honolulu:" + response);
+						System.out.println("Please enter the month this ends in 2 integers followed by the day,"
+								+ "place 0's when necessary, for example July 4 would be 0704");
+						response = scan.nextLine();
+						buffWriter.write(response + "T");
+						/** I need to double check this one*/
+						/*System.out.println("Enter the start time in 4 digits, after 12:00pm is 13:00, " + 
+								"add zeros where necessary");
+						response = scan.nextLine();
+						buffWriter.write(response + "00\n");
+						*/
+						buffWriter.close();
+				   }
+			       
 			   }
 			   catch(FileNotFoundException e) {
 				   System.out.println("oops");
