c91fc979d76a7f1bd24ace893de6c9827ba6c473
lucasaec <lucaseac@hawaii.edu>
2016-02-23 20:41:24 -1000

    Newer version
    This is the newer version

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 
 import java.io.*;
 import java.util.Scanner;
-import java.lang.StringBuilder;
 public class main {
    public static void main(String[]args) {
 	   System.out.println("This software is currently only meant for you to add only one event");
@@ -9,32 +8,28 @@ public class main {
 	   String response;
 	   
 	   /* Used for storing Descriptions */
-	   StringBuilder description = new StringBuilder();
+	   //StringBuilder description = new StringBuilder();
 	   
 	   /*used for storing an option that the user chose*/
-	   int option;
+	   //int option;
 	   
-	   /*
-	   InputStream sstderr = p.getErrorStream();
-	   InputStream sstdout = p.getInputStream();
-	   OutputStream sstdin  = p.getOutputStream();
-	   */
-	   boolean exit = true;
+
+	  // boolean exit = true;
 	   Scanner scan = new Scanner(System.in);
 	   boolean fileMade = false;
 	   
 	   PrintWriter writer;
 	   BufferedWriter buffWriter;
 	   /* Begin running the interface*/
-	   while(exit) {
-		   
+	   //while(exit) {
+		 /*  
 		   System.out.println("\n" + 
                    "Which of these options would you like to add? Press an number and then press enter"
                    + "\n" + "(1) Create an Event"  
 		             + "\n" + "(2) Exit");
-		   
-		   option = scan.nextInt();
-		   if(option == 1) { // Add an event
+		   */
+		   //option = scan.nextInt();
+		 //  if(option == 1) { // Add an event
 			   try {
 				 
 				   
@@ -69,18 +64,21 @@ public class main {
 			           /*date start
 			            * might need to add variables for month and year I don't think 
 			            * events last longer than a month*/
-						System.out.println("Please enter the year this starts in 4 integers");
-						response = scan.nextLine();
-						buffWriter.write("DTSTART;TZID=Pacific/Honolulu:" + response);
-						System.out.println("Please enter the month this starts in 2 integers followed by the day,"
+			           
+					   System.out.println("Please enter the year this starts in 4 integers");
+						
+					   response = scan.nextLine();
+					   buffWriter.write("DTSTART;TZID=Pacific/Honolulu:" + response);
+					   System.out.println("Please enter the month this starts in 2 integers followed by the day,"
 								+ "place 0's when necessary, for example July 4 would be 0704");
-						response = scan.nextLine();
-						buffWriter.write(response + "T");
-						/** I need to double check this one*/
-						System.out.println("Enter the start time in 4 digits, after 12:00pm is 13:00, " + 
-								"add zeros where necessary");
-						response = scan.nextLine();
-						buffWriter.write(response + "00\n");
+					   response = scan.nextLine();
+					  
+					   buffWriter.write(response + "T");
+				       /** I need to double check this one*/
+					   System.out.println("Enter the start time in 4 digits, after 12:00pm is 13:00, " + 
+					       "add zeros where necessary");
+					   response = scan.nextLine();
+					   buffWriter.write(response + "00\n");
 						
 						/*date end*/ 
 						/*System.out.println("Please enter the year this ends in 4 integers");
@@ -97,16 +95,19 @@ public class main {
 						buffWriter.write(response + "00\n");
 						*/
 						buffWriter.close();
+						scan.close();
 				   }
 			       
 			   }
 			   catch(FileNotFoundException e) {
 				   System.out.println("oops");
+				   scan.close();
 				   
 			   } catch (IOException e) {
 				   // TODO Auto-generated catch block
 				   e.printStackTrace();
-		     	}
+				   scan.close();
+		       }
 			   
 			  
 			   
@@ -131,14 +132,14 @@ public class main {
 		    	  
 		       }*/
 			   
-		   }
+		   //}
 		   
 		      
-	       if(option == 2) {
+	       //if(option == 2) {
 	    	   System.out.println("Thank you for using this software");
-	    	   exit = false; 
-	       }
-	   }
+	    	   //exit = false; 
+	      // }
+	   //}
 
 	   
 	   /*    
